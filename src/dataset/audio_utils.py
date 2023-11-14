02f5f088b6ae6baa9da38ed1fb5a3de020d10d6a
Blinorot <pmgrin.work@gmail.com>
2023-11-15 00:39:15 +0100

    Params bug fix

--- src/dataset/audio_utils.py
+++ src/dataset/audio_utils.py
@@ -14,9 +14,9 @@ class AudioFeaturesParams:
     win_size: int = 1024
     hop_size: int = 240
     num_mel_bins: int = 80
-    sampling_rate: int = 16000
+    sampling_rate: int = 24000
     fmin: float = 50.0
-    fmax: Optional[float] = 8000
+    fmax: Optional[float] = 11025.0
     n_fft: int = 2048
     central_padding: bool = False
 
